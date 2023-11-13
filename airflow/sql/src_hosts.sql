f6118c9e728f6934923db2af46f3afd31c5bdac5
atifrani <axeltifrani@gmail.com>
2023-11-13 16:42:00 +0100

    update airflow dag

--- airflow/sql/src_hosts.sql
+++ airflow/sql/src_hosts.sql
@@ -1,5 +1,9 @@
+USE WAREHOUSE COMPUTE_WH;
+USE DATABASE airbnb;
+USE SCHEMA SILVER;
 
-CREATE OR REPLACE TABLE airbnb.src.src_listings_afw
+
+CREATE OR REPLACE TABLE src_listings_afw
                     (listings_id integer,
                     listings_url string,
                     listings_name string,
@@ -11,7 +15,7 @@ CREATE OR REPLACE TABLE airbnb.src.src_listings_afw
                     updated_at datetime);
 
 
-insert into airbnb.src.src_listings_afw 
+insert into src_listings_afw 
                 SELECT  id AS listing_id,  name AS listing_name,  listing_url,  room_type,  minimum_nights,
                 host_id, price AS price_str, created_at, updated_at 
                 FROM airbnb.raw.raw_listings_kst
